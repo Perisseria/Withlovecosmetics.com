f313a277b3d517feed8af4b7287c41aca9595381
Nuwagirajoel <agirayoweri@gmail.com>
2025-08-14 10:46:21 +0300

    firstcommit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Navbar />
+  <landing />
+  <cards />
+  <videopart />
+  <router-view />
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import landing from "./components/landing.vue";
+import cards from "./components/cards.vue";
+import videopart from "./components/videopart.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+    landing,
+    cards,
+    videopart,
+  },
+};
+</script>
---
+++ src/components/videopart.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex justify-center pt-20 w-lg md:w-3/4 aspect-video">
+    <video class="w-2/3" autoplay controls>
+      <source
+        src="https://www.pexels.com/video/1149993/free-video-1149993/"
+        type="video/mp4"
+      />
+      Your browser does not support the video tag.
+    </video>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "videopart",
+};
+</script>
